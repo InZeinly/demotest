7dfcc8414546b5da458a57b1b3d9336574b99ecd
InZeinly <tuzar11@hotmail.com>
2022-09-15 16:32:08 +0300

    asd

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "jenkins_sg" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
-    cidr_blocks      = [var.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -29,7 +29,7 @@ resource "aws_security_group" "jenkins_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [var.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
