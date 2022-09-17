c5adfd4d0e8b3f3b1ba29ab9be5494c958ece065
InZeinly <tuzar11@hotmail.com>
2022-09-17 12:04:41 +0300

    apachetoprod

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "production" {
   key_name = "aws-terraform"
   security_groups = [aws_security_group.jenkins_sg.name]
   # user_data       = "${file("install_jenkins.sh")}"
-  user_data = templatefile("${path.module}/install_docker.sh",{})
+  user_data = templatefile("${path.module}/install_apache.sh",{})
   tags = {
     "Name" = "Production"
   }
